f9658942dc5282219c27094beca235f49397804e
Karl-Knapp <xxtheshadowpantherxx@gmail.com>
2025-09-06 20:20:10 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "https://morganvalleyweb.com", 
+    "https://bthfitness.org", 
 ],
     allow_credentials=True,
     allow_methods=["*"],
